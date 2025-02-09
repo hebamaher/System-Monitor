437f43417aa8f56a5fa2e58b49b7169675a24446
Heba Maher <hebamaher25@gmail.com>
2025-02-09 20:17:55 +0200

    added email feature and modified output format

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-THRESHOLD=80
+THRESHOLD=70
 
 REPORT="system_monitor.log"
 
@@ -11,13 +11,15 @@ echo "----------------------------------" >> "$REPORT"
 echo "Checking Disk Usage.." >> "$REPORT"
 
 check_disk_usage() {
-    disk_usage=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
+    disk_usage=$(df -h / | awk 'NR==1 {print $1, $2, $3} NR==2 {print "", $1, $2, $3}' )
+    echo " $disk_usage%" >> "$REPORT"
+
+    disk_usage_percentage=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
     
-    if [ "$disk_usage" -gt "$THRESHOLD" ]; then
-        echo "Warning: Disk usage is at $disk_usage% (threshold is $THRESHOLD%)" >> "$REPORT"
-	echo "Warning: Disk usage is at $disk_usage% (threshold is $THRESHOLD%)" | mail -s "Disk Alert" "$email"
-    else
-        echo "Disk usage is under control: $disk_usage%" >> "$REPORT"
+    if [ "$disk_usage_percentage" -gt "$THRESHOLD" ]; then
+        msg="Warning: Disk usage is at $disk_usage_percentage% (threshold is $THRESHOLD%)"
+	echo "$msg" >> "$REPORT"
+	echo "$msg" | mail -s "System Monitoring" "$email"
     fi
 }
 
@@ -31,16 +33,6 @@ done
 
 check_disk_usage
 
-while read -r usage partition; do
-	usage=${usage%\%}
-
-	echo "Partition: $partition Usage: $usage%" >> "$REPORT"
-
-	if [ "$usage" -ge "$THRESHOLD" ]; then
-		echo "WARNING: Partition $partition is above ${THRESHOLD}% usage!" >> "$REPORT"
-	fi
-done < <(df -h | awk 'NR>1 {print $5, $6}')
-
 echo "----------------------------------" >> "$REPORT"
 
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
@@ -48,12 +40,15 @@ echo "Current CPU Usage: $cpu_usage%" >> "$REPORT"
 
 echo "-----------------------------------" >> "$REPORT"
 
-memory_usage=$(free -h)
-echo "Memory usage: $memory_usage" >> "$REPORT"
+memory_usage=$(free -h | awk 'NR==1 {print "Memory:\n", $1, $2, $3} NR==2 {print "", $2, $3, $4}')
+echo -e "$memory_usage" >> "$REPORT"
+
 
 echo "-----------------------------------" >> "$REPORT"
 
-processes=$(ps aux --sort=-%mem | head -n 6)
-echo "Memory Consuming Processes: $processes" >> "$REPORT"
+echo "Top 5 Memory-Consuming Processes:" >> "$REPORT"
+echo "PID USER %MEM COMMAND" >> "$REPORT"
+processes=$(ps aux --sort=-%mem | awk 'NR<=6 {printf "%-6s %-10s %-5s %s\n", $2, $1, $4, $11}')
+echo "$processes" >> "$REPORT"
 
 echo "Report saved to $REPORT"
